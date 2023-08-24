5b7e90669ac14dfb38b32a7f829edede486dae3c
mstoliar <mstoliar@softkitit.com>
2023-08-24 12:00:35 +0300

    fix(select): select component

--- app/breeds/BreedsOptions.tsx
+++ app/breeds/BreedsOptions.tsx
@@ -2,36 +2,59 @@
 import React from "react";
 import SelectInput from "../components/shared/inputs/SelectInput";
 
-const limits = [
-  {
-    value: 5,
-    label: "Limit: 5",
-  },
-  {
-    value: 10,
-    label: "Limit: 10",
-  },
-  {
-    value: 15,
-    label: "Limit: 15",
-  },
-  {
-    value: 20,
-    label: "Limit: 20",
-  },
-];
+import styles from "./breeds.module.css";
 
 const BreedsOptions: React.FC = () => {
   const [limitOption, setLimitOption] = React.useState<number>(10);
+  const [breedOption, setBreedOption] = React.useState<string>("all");
 
   const handleLimitChange = (value: number) => {
     setLimitOption(value);
   };
 
-  console.log("limitOption ", limitOption);
+  const handleBreedChange = (value: string) => {
+    setBreedOption(value);
+  };
+
+  console.log("limitOption: ", limitOption);
+  console.log("breedOption: ", breedOption);
+
+  const breeds = [
+    {
+      value: "all",
+      label: "All breeds",
+    },
+  ];
+
+  const limits = [
+    {
+      value: 5,
+      label: "Limit: 5",
+    },
+    {
+      value: 10,
+      label: "Limit: 10",
+    },
+    {
+      value: 15,
+      label: "Limit: 15",
+    },
+    {
+      value: 20,
+      label: "Limit: 20",
+    },
+  ];
 
   return (
     <div>
+      <div style={{ width: 123, height: 40, background: "#f00" }}></div>
+      <SelectInput
+        options={breeds}
+        value={breedOption}
+        onChange={handleBreedChange}
+        className={styles.breeds_select}
+      />
+
       <SelectInput
         options={limits}
         value={limitOption}
--- app/components/shared/inputs/SelectInput.tsx
+++ app/components/shared/inputs/SelectInput.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import styles from "./inputs.module.css";
 
-interface SelectInputProps {
-  options: { value: any; label: string }[];
-  value: number;
-  onChange: (value: number) => void;
+interface SelectInputProps<T> {
+  options: { value: T; label: string }[];
+  value: T;
+  onChange: (value: T) => void;
+  className?: string;
 }
 
-const SelectInput: React.FC<SelectInputProps> = ({
+const SelectInput = <T extends string | number>({
   options,
   value,
   onChange,
-}) => {
+  className,
+}: SelectInputProps<T>) => {
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    onChange(+e.target.value);
+    onChange(e.target.value as T);
   };
 
   return (
-    <select value={value} onChange={handleChange}>
+    <select value={value} onChange={handleChange} className={className}>
       {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.label}
