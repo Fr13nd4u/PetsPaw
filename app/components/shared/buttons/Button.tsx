b3362c4de3f08cb6748ec750efdfdefb750ee6da
mstoliar <mstoliar@softkitit.com>
2023-08-25 15:07:55 +0300

    feat: create breadcrumbs component

--- app/components/shared/buttons/Button.tsx
+++ app/components/shared/buttons/Button.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import styles from "./button.module.css";
 
 interface IButton {
   children: React.ReactNode;
   onClick?: () => void;
+  type: "primary" | "secondary";
 }
 
-const Button: React.FC<IButton> = ({ children }) => {
-  return <button>{children}</button>;
+const Button: React.FC<IButton> = ({ children, onClick, type }) => {
+  const buttonClass = `${styles.btn} ${styles[`btn_${type}`]}`;
+
+  return (
+    <button onClick={onClick} className={buttonClass}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
